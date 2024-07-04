1155f359e5c857686044ca74fb4e2eb9ca5f8c5d
Georgi Georgiev <ghgeorgiev@2create.studio>
2024-07-04 18:33:19 +0300

    Simplify code

--- src/PaymentProfile/PaymentProfile.php
+++ src/PaymentProfile/PaymentProfile.php
@@ -3,52 +3,28 @@
 namespace ANet\PaymentProfile;
 
 use ANet\AuthorizeNet;
+use ANet\CustomerProfile\CustomerProfile;
 use ANet\Exceptions\ANetApiException;
 use DB;
 use Illuminate\Support\Collection;
-use net\authorize\api\contract\v1\ANetApiResponseType;
 use net\authorize\api\contract\v1 as AnetAPI;
-use net\authorize\api\contract\v1\GetCustomerProfileResponse;
+use net\authorize\api\contract\v1\CreateCustomerPaymentProfileResponse;
+use net\authorize\api\contract\v1\CustomerPaymentProfileMaskedType;
 use net\authorize\api\controller as AnetControllers;
 
 class PaymentProfile extends AuthorizeNet
 {
+    /**
+     * @returns Collection<int, CustomerPaymentProfileMaskedType>
+     *
+     * @throws ANetApiException
+     */
     public function get(): Collection
     {
         $customerProfileId = $this->user->getCustomerProfileId();
-        $merchantKeys = $this->getMerchantAuthentication();
+        $customerProfile = app(CustomerProfile::class, ['user' => $this->user])->getById($customerProfileId);
 
-        // Create the API request
-        $request = new AnetAPI\GetCustomerPaymentProfileRequest();
-        $request->setMerchantAuthentication($merchantKeys);
-        $request->setCustomerProfileId($customerProfileId);
-
-        // Create the controller
-        $controller = new AnetControllers\GetCustomerProfileController($request);
-
-        // Execute the request
-        /** @var GetCustomerProfileResponse $response */
-        $response = $this->execute($controller);
-
-        if (empty($response)) {
-            throw new ANetApiException($response);
-        }
-
-        $message = $response->getMessages()->getMessage()[0] ?? null;
-
-        // Customer has no saved payment profiles yet
-        if ($message?->getCode() === 'E00121') {
-            return collect();
-        }
-
-        if ($response->getMessages()->getResultCode() == 'Ok') {
-            $profile = $response->getProfile();
-            $paymentProfiles = $profile->getPaymentProfiles();
-
-            return collect($paymentProfiles);
-        }
-
-        return collect();
+        return collect($customerProfile->getPaymentProfiles());
     }
 
     public function create(array $opaqueData, array $source, ?AnetAPI\CustomerAddressType $address = null)
@@ -81,6 +57,7 @@ class PaymentProfile extends AuthorizeNet
         $paymentProfileRequest->setPaymentProfile($customerPaymentProfileType);
 
         // Create the controller and get the response
+        /** @var ?CreateCustomerPaymentProfileResponse $response */
         $controller = new AnetControllers\CreateCustomerPaymentProfileController($paymentProfileRequest);
         $response = $this->execute($controller);
 
@@ -91,20 +68,18 @@ class PaymentProfile extends AuthorizeNet
         return $response;
     }
 
-    protected function storeInDatabase(ANetApiResponseType $response, iterable $source): void
+    protected function storeInDatabase(CreateCustomerPaymentProfileResponse $response, iterable $source): void
     {
-        DB::transaction(function () use ($response, $source) {
-            DB::table('user_payment_profiles')->updateOrInsert(
-                [
-                    'user_id' => $this->user->getUserIdForAnet(),
-                    'payment_profile_id' => $response->getCustomerPaymentProfileId(),
-                ],
-                [
-                    'last_4' => $source['last_4'],
-                    'brand' => $source['brand'],
-                    'type' => $source['type'],
-                ],
-            );
-        });
+        DB::table('user_payment_profiles')->upsert(
+            values: [[
+                'last_4' => $source['last_4'],
+                'brand' => $source['brand'],
+                'type' => $source['type'],
+                'user_id' => $this->user->getUserIdForAnet(),
+                'payment_profile_id' => $response->getCustomerPaymentProfileId(),
+            ]],
+            uniqueBy: ['payment_profile_id'],
+            update: ['last_4', 'brand', 'type', 'user_id', 'payment_profile_id'],
+        );
     }
 }
